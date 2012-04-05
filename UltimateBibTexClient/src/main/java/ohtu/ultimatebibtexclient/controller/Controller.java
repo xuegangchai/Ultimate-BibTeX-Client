9d607d5db1b38cfd9a6e53615748b19b2506f533
Chai Xuegang <xuegang.chai@helsinki.fi>
2012-04-05 21:47:35 +0300

    Controlleri vahan muokattu, virheita vaheni, rivilla 17 viela yksi virhe

--- UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
+++ UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
@@ -4,35 +4,47 @@
  */
 package ohtu.ultimatebibtexclient.controller;
 
+/**
+ *
+ * @author chai
+ */
+import ohtu.ultimatebibtexclient.service.ReferenceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Controller {
-    
-    @RequestMapping (value = "vihaiset", method = "GET")
-    public String listReferences (){
-        
+
+    @Autowired
+    private ReferenceService ReferenceService;
+
+    @RequestMapping(value = "vihaiset", method = RequestMethod.GET)
+    public String listReferences() {
+        // ReferenceService.fetch(); //?? en oo varma
+        return "redirect:/vihaiset";
     }
 
-
-    @RequestMapping (value = "vihaiset/create.html", method = "GET")
-    public String showCreationForm (){
+    @RequestMapping(value = "vihaiset/create.html", method = RequestMethod.GET)
+    public String showCreationForm() {
+        return "";
     }
 
-
-    @RequestMapping (value = "vihaiset/reference", method = "POST")
-    public String createReference (){
+    @RequestMapping(value = "vihaiset/reference", method = RequestMethod.POST)
+    public String createReference() {
+        //ReferenceService.create(); //?? en oo varma
+        return "redirect:/vihaiset";
     }
 
-
-    @RequestMapping (value = "vihaiset/reference/{refID}", method = "GET")
-    public String showModificationForm (@PathVariable int refID){
+    @RequestMapping(value = "vihaiset/reference/{refID}", method = RequestMethod.GET)
+    public String showModificationForm(@PathVariable int refID) {
+        return "";
     }
 
-
-    @RequestMapping (value = "vihaiset/reference/{refID}", method = "POST")
-    public String modifyReference (@PathVariable int refID){
+    @RequestMapping(value = "vihaiset/reference/{refID}", method = RequestMethod.POST)
+    public String modifyReference(@PathVariable int refID) {
+        //ReferenceService.modify(Reference); //?? en oo varma
+        return "redirect:/vihaiset";
     }
 }
