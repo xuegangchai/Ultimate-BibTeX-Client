c16e83e0ab479e6f3c45e045d50c152f3a29b366
Chai Xuegang <xuegang.chai@helsinki.fi>
2012-04-05 21:43:18 +0300

    muuttujat aluksi id ja nimi

--- UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/domain/Reference.java
+++ UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/domain/Reference.java
@@ -6,8 +6,38 @@ package ohtu.ultimatebibtexclient.domain;
 
 /**
  *
- * @author ewk
+ * @author chai
  */
-public class Reference {
-    
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Reference implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int id;
+    private String nimi;
+
+    public Reference() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
 }
