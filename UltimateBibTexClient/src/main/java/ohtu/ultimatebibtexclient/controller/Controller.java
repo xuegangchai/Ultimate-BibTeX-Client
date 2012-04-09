f0187c31bd03ebf91046034cbc42c961cdb6ced9
Tuukka Norri <tsnorri@iki.fi>
2012-04-10 01:12:39 +0300

    Modifying and creating references

--- UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
+++ UltimateBibTexClient/src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
@@ -8,23 +8,28 @@ package ohtu.ultimatebibtexclient.controller;
  *
  * @author chai
  */
+import java.lang.reflect.InvocationTargetException;
+import javax.servlet.http.HttpServletRequest;
 import ohtu.ultimatebibtexclient.domain.Reference;
 import ohtu.ultimatebibtexclient.service.ReferenceService;
+import ohtu.ultimatebibtexclient.util.ReferenceValueAssigner;
 import ohtu.ultimatebibtexclient.util.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@org.springframework.stereotype.Controller
-public class Controller {
 
-    @Autowired
-    private ReferenceService referenceService;
+@org.springframework.stereotype.Controller
+public class Controller
+{
+    @Autowired private ReferenceService referenceService;
+	@Autowired private ReferenceValueAssigner valueAssigner;
+		
 	
-	
-    @RequestMapping(value = "vihaiset", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.GET)
     public String listReferences(Model model)
 	{
 		model.addAttribute ("references", referenceService.fetch ());
@@ -32,24 +37,27 @@ public class Controller {
     }
 
 	
-    @RequestMapping(value = "vihaiset/create.html", method = RequestMethod.GET)
+    @RequestMapping(value = "create.html", method = RequestMethod.GET)
     public String showCreationForm(Model model)
 	{
 		model.addAttribute ("title", "Luo uusi viite");
-		model.addAttribute ("action", "vihaiset/reference");
+		model.addAttribute ("action", "reference");
         return "modify";
     }
 
 	
-    @RequestMapping(value = "vihaiset/reference", method = RequestMethod.POST)
-    public String createReference()
+	@Transactional
+    @RequestMapping(value = "reference", method = RequestMethod.POST)
+    public String createReference(HttpServletRequest req) throws IllegalAccessException, InvocationTargetException
 	{
-        //ReferenceService.create(); //?? en oo varma
-        return "redirect:/vihaiset";
+        Reference ref = new Reference ();
+		valueAssigner.assignReferenceVariables (req, ref);
+		referenceService.modify (ref);
+        return "redirect:/";
     }
 
 	
-    @RequestMapping(value = "vihaiset/reference/{refID}", method = RequestMethod.GET)
+    @RequestMapping(value = "reference/{refID}", method = RequestMethod.GET)
     public String showModificationForm(Model model, @PathVariable Integer refID)
 	{
 		Reference ref = referenceService.fetchByID (refID);
@@ -57,15 +65,21 @@ public class Controller {
 			throw new ResourceNotFoundException ();
 		
 		model.addAttribute ("title", "Muokkaa viitettä");
-		model.addAttribute ("action", String.format ("vihaiset/reference/%d", refID));
+		model.addAttribute ("action", String.format ("reference/%d", refID));
 		model.addAttribute ("ref", ref);
         return "modify";
     }
 
 	
-    @RequestMapping(value = "vihaiset/reference/{refID}", method = RequestMethod.POST)
-    public String modifyReference(@PathVariable int refID) {
-        //ReferenceService.modify(Reference); //?? en oo varma
-        return "redirect:/vihaiset";
+	@Transactional
+    @RequestMapping(value = "reference/{refID}", method = RequestMethod.POST)
+    public String modifyReference(HttpServletRequest req, @PathVariable int refID) throws IllegalAccessException, InvocationTargetException
+	{
+		Reference ref = referenceService.fetchByID (refID);
+		if (null == ref)
+			throw new ResourceNotFoundException ();
+		valueAssigner.assignReferenceVariables (req, ref);
+		referenceService.modify (ref);
+        return "redirect:/";
     }
 }
