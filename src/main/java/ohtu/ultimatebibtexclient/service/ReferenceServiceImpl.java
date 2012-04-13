e2e685afc751c1a9d4f6e468f8de082d58ee4e06
Chai Xuegang <xuegang.chai@helsinki.fi>
2012-04-13 16:56:46 +0300

    sisennykset korjattu

--- src/main/java/ohtu/ultimatebibtexclient/service/ReferenceServiceImpl.java
+++ src/main/java/ohtu/ultimatebibtexclient/service/ReferenceServiceImpl.java
@@ -4,6 +4,7 @@
  */
 package ohtu.ultimatebibtexclient.service;
 
+
 import java.util.Collection;
 import ohtu.ultimatebibtexclient.domain.Reference;
 import ohtu.ultimatebibtexclient.repository.ReferenceRepository;
@@ -12,45 +13,57 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+
 /**
  *
  * @author tsnorri
  */
 @Service
-public class ReferenceServiceImpl implements ReferenceService {
+public class ReferenceServiceImpl implements ReferenceService
+{
+	@Autowired ReferenceRepository referenceRepository;
+	
+	
+	public ReferenceServiceImpl ()
+	{
+	}
+	
+	
+	@Override
+    @Transactional (readOnly = true)
+	public Collection<Reference> fetch ()
+	{
+		return referenceRepository.findAll ();
+	}
 
-    @Autowired
-    ReferenceRepository referenceRepository;
 
-    public ReferenceServiceImpl() {
-    }
+	@Override
+	@Transactional (readOnly = true)
+	public Reference fetchByID (Integer id)
+	{
+		return referenceRepository.findOne (id);
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Collection<Reference> fetch() {
-        return referenceRepository.findAll();
-    }
 
-    @Override
-    @Transactional(readOnly = true)
-    public Reference fetchByID(Integer id) {
-        return referenceRepository.findOne(id);
-    }
+	@Override
+	@Transactional (propagation = Propagation.REQUIRED)
+	public Reference modify (Reference ref)
+	{
+		return referenceRepository.save (ref);
+	}
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED)
-    public Reference modify(Reference ref) {
-        return referenceRepository.save(ref);
-    }
 
-    @Override
-    @Transactional(propagation = Propagation.REQUIRED)
-    public void delete(Reference ref) {
-        referenceRepository.delete(ref);
-    }
+	@Override
+	@Transactional (propagation = Propagation.REQUIRED)
+	public void delete (Reference ref)
+	{
+		referenceRepository.delete (ref);
+	}
 
-    @Override
-    public long count() {
-        return referenceRepository.count();
-    }
+
+	@Override
+	public long count ()
+	{
+		return referenceRepository.count ();
+	}
 }
