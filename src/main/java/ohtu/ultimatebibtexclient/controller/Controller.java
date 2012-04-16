f0aabfc0cfd70516591e2b98ff29f8cb296177f3
Tuukka Norri <tsnorri@iki.fi>
2012-04-16 16:33:12 +0300

    Controller method for downloading BibTeX

--- src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
+++ src/main/java/ohtu/ultimatebibtexclient/controller/Controller.java
@@ -4,14 +4,21 @@
  */
 package ohtu.ultimatebibtexclient.controller;
 
+
 /**
  *
  * @author chai
  */
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import ohtu.ultimatebibtexclient.domain.Reference;
 import ohtu.ultimatebibtexclient.service.ReferenceService;
+import ohtu.ultimatebibtexclient.util.BibtexWriter;
+import ohtu.ultimatebibtexclient.util.BibtexWriterImpl;
 import ohtu.ultimatebibtexclient.util.ReferenceValueAssigner;
 import ohtu.ultimatebibtexclient.util.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,40 +28,50 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@org.springframework.stereotype.Controller
-public class Controller {
 
+@org.springframework.stereotype.Controller
+public class Controller
+{
     @Autowired
     private ReferenceService referenceService;
     @Autowired
     private ReferenceValueAssigner valueAssigner;
 
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String listReferences(Model model) {
+    public String listReferences(Model model)
+    {
         model.addAttribute("references", referenceService.fetch());
         return "list";
     }
 
+
     @RequestMapping(value = "create.html", method = RequestMethod.GET)
-    public String showCreationForm(Model model) {
+    public String showCreationForm(Model model)
+    {
         model.addAttribute("title", "Luo uusi viite");
         model.addAttribute("action", "reference");
         return "modify";
     }
 
+
     @Transactional
     @RequestMapping(value = "reference", method = RequestMethod.POST)
-    public String createReference(HttpServletRequest req) throws IllegalAccessException, InvocationTargetException {
+    public String createReference(HttpServletRequest req) throws IllegalAccessException, InvocationTargetException
+    {
         Reference ref = new Reference();
         valueAssigner.assignReferenceVariables(req, ref);
         referenceService.modify(ref);
         return "redirect:/";
     }
 
+
     @RequestMapping(value = "reference/{refID}", method = RequestMethod.GET)
-    public String showModificationForm(Model model, @PathVariable Integer refID) {
+    public String showModificationForm(Model model, @PathVariable Integer refID)
+    {
         Reference ref = referenceService.fetchByID(refID);
-        if (null == ref) {
+        if (null == ref)
+        {
             throw new ResourceNotFoundException();
         }
 
@@ -64,15 +81,30 @@ public class Controller {
         return "modify";
     }
 
+
     @Transactional
     @RequestMapping(value = "reference/{refID}", method = RequestMethod.POST)
-    public String modifyReference(HttpServletRequest req, @PathVariable int refID) throws IllegalAccessException, InvocationTargetException {
+    public String modifyReference(HttpServletRequest req, @PathVariable int refID) throws IllegalAccessException, InvocationTargetException
+    {
         Reference ref = referenceService.fetchByID(refID);
-        if (null == ref) {
+        if (null == ref)
+        {
             throw new ResourceNotFoundException();
         }
         valueAssigner.assignReferenceVariables(req, ref);
         referenceService.modify(ref);
         return "redirect:/";
+    }
+    
+    
+    @RequestMapping(value = "bibtex", method = RequestMethod.GET)
+    public void fetchReferencesAsBibtex(HttpServletResponse response) throws IOException
+    {
+        BibtexWriter writer = new BibtexWriterImpl();
+        Collection<Reference> refs = referenceService.fetch();
+        ServletOutputStream outputStream = response.getOutputStream();
+        
+        response.setHeader("Content-Type", "text/x-bibtex");
+        writer.write(refs, outputStream);
     }
 }
