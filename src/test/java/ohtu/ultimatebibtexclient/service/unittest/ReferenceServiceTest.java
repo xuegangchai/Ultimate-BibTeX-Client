f46f986c33fb2aa23202e19f75364872b59d03e9
Tuukka Norri <tsnorri@iki.fi>
2012-04-21 14:56:14 +0300

    Re-indent

--- src/test/java/ohtu/ultimatebibtexclient/service/unittest/ReferenceServiceTest.java
+++ src/test/java/ohtu/ultimatebibtexclient/service/unittest/ReferenceServiceTest.java
@@ -19,75 +19,75 @@ import org.junit.*;
  */
 public class ReferenceServiceTest extends SpringTestBase
 {
-	private ReferenceService svc;
+    private ReferenceService svc;
 
 
-	@BeforeClass
-	public static void setUpClass () throws Exception
-	{
-	}
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+    }
 
 
-	@AfterClass
-	public static void tearDownClass () throws Exception
-	{
-	}
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
 
 
-	@Before
-	public void setUp ()
-	{
-		svc = applicationContext.getBean (ReferenceService.class);
-		assertNotNull (svc);
-	}
+    @Before
+    public void setUp()
+    {
+        svc = applicationContext.getBean(ReferenceService.class);
+        assertNotNull(svc);
+    }
 
 
-	@After
-	public void tearDown ()
-	{
-		assertEquals (0, svc.count ());
-	}
+    @After
+    public void tearDown()
+    {
+        assertEquals(0, svc.count());
+    }
 
 
-	@Test
-	public void testCreate ()
-	{
-		Reference ref = new Reference ();
-		ref = svc.modify (ref);
-		assertNotNull (ref);
-		
-		assertNotNull (svc.fetchByID (ref.getId ()));
-		
-		svc.delete (ref);
-	}
-	
-	
-	@Test
-	public void testFetchAll ()
-	{
-		Reference ref1 = svc.modify (new Reference ());
-		Reference ref2 = svc.modify (new Reference ());
-		assertNotNull (ref1);
-		assertNotNull (ref2);
-		
-		Collection<Reference> refs = svc.fetch ();
-		assertEquals (2, refs.size ());
-		
-		svc.delete (ref1);
-		svc.delete (ref2);
-	}
-	
-	
-	@Test
-	public void testModify ()
-	{
-		Reference ref = svc.modify (new Reference ());
-		assertNotNull (ref);
-		
-		ref.setKey ("test");
-		svc.modify (ref);
-		assertEquals ("test", svc.fetchByID (ref.getId ()).getKey ());
-		
-		svc.delete (ref);
-	}
+    @Test
+    public void testCreate()
+    {
+        Reference ref = new Reference();
+        ref = svc.modify(ref);
+        assertNotNull(ref);
+
+        assertNotNull(svc.fetchByID(ref.getId()));
+
+        svc.delete(ref);
+    }
+
+
+    @Test
+    public void testFetchAll()
+    {
+        Reference ref1 = svc.modify(new Reference());
+        Reference ref2 = svc.modify(new Reference());
+        assertNotNull(ref1);
+        assertNotNull(ref2);
+
+        Collection<Reference> refs = svc.fetch();
+        assertEquals(2, refs.size());
+
+        svc.delete(ref1);
+        svc.delete(ref2);
+    }
+
+
+    @Test
+    public void testModify()
+    {
+        Reference ref = svc.modify(new Reference());
+        assertNotNull(ref);
+
+        ref.setKey("test");
+        svc.modify(ref);
+        assertEquals("test", svc.fetchByID(ref.getId()).getKey());
+
+        svc.delete(ref);
+    }
 }
